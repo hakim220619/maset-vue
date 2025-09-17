834fe1215240b0843dd705b306182595e891badf
zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2025-09-17 11:09:58 +0700

    Update environment variables and improve deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build -t zulkarnen/maset-vue -f .docker/Dockerfile --target production . && \
 docker push zulkarnen/maset-vue && \
 ssh developer@103.181.182.81 "cd /home/developer/app/web/ && docker compose pull && docker compose up -d"
